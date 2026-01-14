178cd7a7159d8b6435166fe5e4e4a1d36877e644
MelcherMate <melchermate28@gmail.com>
2026-01-14 16:18:49 +0100

    small change to ex 15

--- MCI/heat_and_mass_transfer/Melcher_M_Heat_and_Mass_Transfer_ExerciseNr_15.m
+++ MCI/heat_and_mass_transfer/Melcher_M_Heat_and_Mass_Transfer_ExerciseNr_15.m
@@ -1,42 +1,43 @@
-function Exercise_15
-    % Data
-    m_dot_o = 3000/3600; % kg/s (oil)
-    cp_o = 2000; % J/kg/K
-    m_dot_c = 0.6; % kg/s (water)
-    cp_c = 4000; % J/kg/K
-    U = 220.75; % W/m^2/K
-    A_tot = 3.88; % m^2
+% exercise15_he_cc_operation.m
+% Parts a) b) and c) are done on paper
 
-    Th_in  = 91; % oil in [°C]
-    Th_out = 62; % oil out [°C]
-    Tc_in  = 10; % water in [°C]
-    Tc_out = 30; % water out[°C]
+% Given data
+m_dot_o = 3000/3600; % kg/s (oil)
+cp_o = 2000; % J/kg/K
+m_dot_c = 0.6; % kg/s (water)
+cp_c = 4000; % J/kg/K
+U = 220.75; % W/m^2/K
+A_tot = 3.88; % m^2
 
-    C_h = m_dot_o*cp_o; % W/K
-    C_c = m_dot_c*cp_c; % W/K
+Th_in  = 91; % oil in [°C]
+Th_out = 62; % oil out [°C]
+Tc_in  = 10; % water in [°C]
+Tc_out = 30; % water out[°C]
 
-    % Effectiveness-NTU method gives exponential profiles for constant U
-    NTU = U*A_tot/min(C_h,C_c)            
-    x = linspace(0,1,200);            
-    A = x*A_tot;
+C_h = m_dot_o*cp_o; % W/K
+C_c = m_dot_c*cp_c; % W/K
 
-    % Temperature difference at one end:
-    DT1 = Th_in - Tc_out;
-    DT2 = Th_out - Tc_in;
+% Effectiveness-NTU method gives exponential profiles for constant U
+NTU = U*A_tot/min(C_h,C_c)            
+x = linspace(0,1,200);            
+A = x*A_tot;
 
-    % Local delta-T along the exchanger (counter-current):
-    theta = DT1 - (DT1-DT2).*x;
+% Temperature difference at one end:
+DT1 = Th_in - Tc_out;
+DT2 = Th_out - Tc_in;
 
-    % Reconstruct local hot and cold temperatures:
-    Th = Th_in - (Th_in-Th_out).*x;
-    Tc = Th - theta;
+% Local delta-T along the exchanger (counter-current):
+theta = DT1 - (DT1-DT2).*x;
 
-    % Plot
-    figure; hold on; box on;
-    plot(A,Th,'r','LineWidth',2);
-    plot(A,Tc,'b','LineWidth',2);
-    xlabel('Heat-transfer area A [m^2]');
-    ylabel('Temperature [°C]');
-    legend('Oil (hot)','Water (cold)','Location','best');
-    title('Counter-current heat exchanger temperature profiles');
-end
+% Reconstruct local hot and cold temperatures:
+Th = Th_in - (Th_in-Th_out).*x;
+Tc = Th - theta;
+
+% Plot
+figure; hold on; box on;
+plot(A,Th,'r','LineWidth',2);
+plot(A,Tc,'b','LineWidth',2);
+xlabel('Heat-transfer area A [m^2]');
+ylabel('Temperature [°C]');
+legend('Oil (hot)','Water (cold)','Location','best');
+title('Counter-current heat exchanger temperature profiles');
